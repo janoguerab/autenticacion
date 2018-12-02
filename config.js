de58f238b9f32eb43263bd78697316e4a5606ecf
Alejandro Noguera <janoguerab@unal.edu.co>
2018-12-02 18:48:13 -0500

    autenticacion con token sin refrestoken

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-
 module.exports = {
-    port: process.env.PORT || 3000,
-    db: process.env.MONGODB || "mongodb+srv://cluster0-sluke.mongodb.net/test?retryWrites=true",
-    auth: {
-        user: process.env.USERDB || "janoguerab",
-        password: process.env.PASSDB || "@dm1NM4ron7"
+  port: process.env.PORT || 3000,
+  db: process.env.MONGODB || 'mongodb+srv://cluster0-sluke.mongodb.net/test?retryWrites=true',
+  auth: {
+      user: process.env.USERDB || 'janoguerab',
+      password: process.env.PASSDB || '@dm1NM4ron7'
     },
-    SECRET_TOKEN: 'miclavedetokens'
-
-}
+  SECRET_TOKEN: 'miclavedetokens'
+};
